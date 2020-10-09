41db990e8f226c18150cb656f69aae29f9eef03e
ARManakhov <sheih000@gmail.com>
2020-10-10 01:23:33 +0300

    implemented device token generation

--- webapp/src/main/java/dev/elektronika/meteoradar/services/impl/DeviceServiceImpl.java
+++ webapp/src/main/java/dev/elektronika/meteoradar/services/impl/DeviceServiceImpl.java
@@ -1,17 +1,24 @@
 package dev.elektronika.meteoradar.services.impl;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import dev.elektronika.meteoradar.model.Device;
 import dev.elektronika.meteoradar.model.User;
 import dev.elektronika.meteoradar.repository.DeviceRepository;
 import dev.elektronika.meteoradar.services.DeviceService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
 public class DeviceServiceImpl implements DeviceService {
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey = "secret";
 
     DeviceRepository deviceRepository;
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
     public DeviceServiceImpl(DeviceRepository deviceRepository) {
         this.deviceRepository = deviceRepository;
@@ -20,14 +27,19 @@ public class DeviceServiceImpl implements DeviceService {
     @Override
     public Device newDevice(User owner, String name) {
         Device device = Device.builder().name(name).owner(owner).build();
+        saveDevice(device);
         generateToken(device);
         saveDevice(device);
         return device;
     }
 
-    private void generateToken(Device device){
-        //todo generate
-
+    private void generateToken(Device device){ //todo impl in separated method due security issues
+        Date now = new Date();
+        String token = JWT.create()
+                .withClaim("deviceId",device.getId())
+                .withClaim("ownerId",device.getOwner().getId())
+                .withIssuedAt(now).sign(algorithm);
+        device.setToken(token);
     }
 
     @Override
